6d8054c8c7cb70add6ec9f88e91dd1ec1ff1c58a
Charles Yinchu Dong <charlesd@Charless-MacBook-Pro.local>
2019-03-24 21:15:00 -0700

    Finish week 3.

--- algorithms/course-4-shorted-path-revisit/scriptalgorithms.py
+++ algorithms/course-4-shorted-path-revisit/scriptalgorithms.py
@@ -10,6 +10,7 @@ import copy
 from collections import Counter
 import sys, threading
 import heapq
+import matplotlib.pyplot as plt
 
 def week1():
     """
@@ -82,6 +83,7 @@ def week2():
     """
     ### Initialization
     fileLocation = 'week-2/tsp.txt'
+#    fileLocation = 'week-2/tsp-v1.txt'
 
     ### Load data
     with open(fileLocation, 'r') as f:
@@ -91,11 +93,31 @@ def week2():
     dataV2 = [[float(y) for y in x] for x in dataV1]
 
 #    print(dataV2)
-    
-    minDist = tsp(numCity, dataV2)
+#    plotCities(dataV2)    
+    minDist1 = tsp(13, dataV2[:13])
+    minDist2 = tsp(14, dataV2[11:])
+    a1 = dataV2[11]
+    a2 = dataV2[12]
+    overlap = np.sqrt(((a1[0] - a2[0]) ** 2) + ((a1[1] - a2[1]) ** 2))
 
-    print(minDist)
+    print(minDist1)
+    print(minDist2)
+    print(overlap)
+    print('final: ', minDist1 + minDist2 - 2* overlap)
  
+def plotCities(data):
+    """
+    Plot city locations for TSP problem
+    """
+    X = [x[0] for x in data]
+    Y = [x[1] for x in data]
+    label = list(range(len(data)))
+    fig, ax = plt.subplots()
+    ax.scatter(X, Y)
+    for i in label:
+        ax.annotate(i, (X[i], Y[i]))
+    plt.show()
+
 def tsp(N, data):
     """
     Find the minimum path for Traveling Salesman Problem
@@ -107,28 +129,111 @@ def tsp(N, data):
         stringToList[0] = '1' 
         nodes = [i for i,v in enumerate(stringToList) if v == '1']
         return nodes
+    ### Define function to convert list of nodes to row number
+    def nodesToRowIdx(N, nodes):
+        nodesNoFirst = [x for x in nodes if x != 0]
+        rowIdx = 0
+        for node in nodesNoFirst:
+            rowIdx += 2 ** (N - node - 1)
+        return rowIdx
     ### Define function to calculate distance
     def getDist(a1, a2):
         dist = np.sqrt(((a1[0] - a2[0]) ** 2) + ((a1[1] - a2[1]) ** 2))
         return dist
     
     ### Init
-#    A = np.full((2 ** (N-1), N), np.inf)
-#    A[0, 0] = 0
-#    for m in range(2, N+1):
-    for m in range(3,4):
+    A = np.full((2 ** (N-1), N), np.inf)
+    A[0, 0] = 0
+    for m in range(2, N+1):
+#    for m in range(3,4):
         possibleSs = []
-        for i in range(2 ** 24):
+        for i in range(2 ** (N-1)):
             nodes = rowIdxToNodes(N, i)
             if len(nodes) == m:
                 possibleSs.append(nodes)
-        print(possibleSs)
+#        print(m)
+#        print(possibleSs)
+        for possibleS in possibleSs:
+            for j in possibleS:
+                if j != 0:
+                    minVal = np.inf
+                    rowIdxWithJ = nodesToRowIdx(N, possibleS)
+                    rowIdxNoJ = nodesToRowIdx(N, [x for x in possibleS if x != j])
+                    for k in possibleS:
+                        if k != j:
+                            distKJ = getDist(data[k], data[j])
+                            minVal = min(minVal, A[rowIdxNoJ, k] + distKJ)
+                    A[rowIdxWithJ, j] = minVal
+    minDist = np.inf
+    for j in range(1, N):
+        minDist = min(minDist, A[2 ** (N-1) - 1, j] + getDist(data[j], data[0]))
 
-    return 1 
+    return minDist 
+
+def week3():
+    """
+    Code up Traveling Salesman Problem using greedy algorithm for week 3
+    """
+    ### Initialization
+    fileLocation = 'week-3/nn.txt'
+#    fileLocation = 'week-3/nn-test.txt'
+
+    ### Load data
+    with open(fileLocation, 'r') as f:
+        dataRaw = f.read().splitlines()
+    numCity = int(dataRaw[0])
+    dataV1 = [x.split() for x in dataRaw[1:]]
+    dataV2 = [[float(y) for y in x] for x in dataV1]
+
+#    print(dataV2)
+
+    ### Apply greedy algorithm
+    minDist = tspGreedy(numCity, dataV2)
+
+    print(minDist)
+
+def tspGreedy(N, data):
+    """
+    Greedy algorithm for tsp problem
+    """
+    def getDist(a1, a2):
+        dist = np.sqrt(((a1[2] - a2[2]) ** 2) + ((a1[1] - a2[1]) ** 2))
+        return dist
+ 
+    ### Init
+    totalDist = 0
+    pointOrigin = data[0]
+    pointAIdx = 1
+    pointA = data[0]
+    while len(data) != 1:
+        localDistMin = np.inf
+        for point in data:
+            if point[0] != pointAIdx:
+                tempDist = getDist(point, pointA)
+                if tempDist < localDistMin:
+                    localDistMin = tempDist
+                    pointBIdx = point[0]
+                    pointB = point
+                elif tempDist == localDistMin:
+                    if point[0] < pointBIdx:
+                        pointBIdx = point[0]
+                        pointB = point
+        totalDist += localDistMin
+        data = [x for x in data if x[0] != float(pointAIdx)]
+        pointAIdx = pointBIdx
+        pointA = pointB
+#        print(totalDist)
+#        print(pointAIdx)
+#        print(pointA)
+#        print(data)
+    totalDist += getDist(pointOrigin, pointA)
+
+    return totalDist      
 
 if __name__ == '__main__':
 #    week1()
-    week2()
+#    week2()
+    week3()
 
 
 
